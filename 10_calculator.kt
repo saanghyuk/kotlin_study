07e530cd3ab8a0844b38644627a5dff4556178b5
saanghyuk <saanghyuk@gmail.com>
2023-03-09 23:58:25 +0900

    20230308

---
+++ 10_calculator.kt
@@ -0,0 +1,29 @@
+/**
+ * You can edit, run, and share this code.
+ * play.kotlinlang.org
+ */
+fun main() {
+
+	remainder(10, 4)
+
+}
+
+fun sumTwo(num1 : Int, num2 : Int){
+        println(num1 + num2)
+    }
+
+fun sumThree(num1 : Int, num2 : Int, num3 : Int){
+        println(num1 + num2 + num3)
+}
+
+fun minus(num1: Int, num2: Int){
+    println(num1 - num2)
+}
+
+fun division(num1: Int, num2: Int){
+    println(num1 / num2)
+}
+
+fun remainder(num1: Int, num2: Int){
+    println(num1 % num2)
+}
---
+++ 9_function.kt
@@ -0,0 +1,17 @@
+/**
+ * You can edit, run, and share this code.
+ * play.kotlinlang.org
+ */
+fun main() {
+
+    val result = sum(10, 20);
+    println(result)
+}
+
+fun sum(a: Int, b: Int): Int{
+
+    // println(a+b)
+    val result = a+b;
+
+    return result
+}
